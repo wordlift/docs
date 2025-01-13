74839e24ff6e54c1ee1a58dd2f2f75489fe934a6
mauanga <mauro.anganuzzi@gmail.com>
2025-01-13 11:22:30 +0100

    fix: update agent.yaml to include endpoint metadata

--- docs/api/agent/sidebar.js
+++ docs/api/agent/sidebar.js
@@ -1,1 +1,1 @@
-module.exports = [{"type":"doc","id":"api/agent/wordlift-agent-api"},{"type":"category","label":"UNTAGGED","items":[{"type":"doc","id":"api/agent/ask-request-api-ask-post","label":"Ask Request","className":"api-method post"}]}];
+module.exports = [{"type":"doc","id":"api/agent/wordlift-agent-api"},{"type":"category","label":"Agent","items":[{"type":"doc","id":"api/agent/ask-request-api-ask-post","label":"Ask Request","className":"api-method post"}]}];
