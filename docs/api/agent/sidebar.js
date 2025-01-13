08f9121ed7795f2bd50e4173547240ef4193b1c5
mauanga <mauro.anganuzzi@gmail.com>
2025-01-13 10:31:00 +0100

    add agent api documentation

---
+++ docs/api/agent/sidebar.js
@@ -0,0 +1,1 @@
+module.exports = [{"type":"doc","id":"api/agent/wordlift-agent-api"},{"type":"category","label":"UNTAGGED","items":[{"type":"doc","id":"api/agent/ask-request-api-ask-post","label":"Ask Request","className":"api-method post"}]}];
