b705d68d97c5c50caa23ad901b305763e6317a82
cyberandy <andrea@insideout.io>
2025-02-03 11:21:07 +0100

    adding diagram

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -28,6 +28,10 @@ const config = {
     defaultLocale: "en",
     locales: ["en"],
   },
+  // Enable Mermaid support in markdown.
+  markdown: {
+    mermaid: true,
+  },
   scripts: [
     {
       src: 'https://cdn.zapier.com/packages/partner-sdk/v0/zapier-elements/zapier-elements.esm.js',
@@ -503,6 +507,7 @@ const config = {
   ],
   themes: [
     "docusaurus-theme-openapi-docs",
+    "@docusaurus/theme-mermaid",
     // Remove this entire block:
     // [
     //   "@docusaurus/theme-classic",
