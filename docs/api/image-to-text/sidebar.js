6f6328d9d67ddb75ece6cc25d7d2ea9b98647e58
cyberandy <andrea@insideout.io>
2025-03-25 10:25:32 +0100

    update api

--- docs/api/image-to-text/sidebar.js
+++ docs/api/image-to-text/sidebar.js
@@ -1,3 +1,18 @@
 module.exports = [
-    "wordlift-image-to-text-api",
+    {
+        "type": "doc",
+        "id": "api/image-to-text/wordlift-image-to-text-api"
+    },
+    {
+        "type": "category",
+        "label": "Image-to-Text",
+        "items": [
+            {
+                "type": "doc",
+                "id": "api/image-to-text/image-to-text-api-image-2-text-v1-post",
+                "label": "Convert Image to Text",
+                "className": "api-method post"
+            }
+        ]
+    }
 ];
