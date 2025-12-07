5109afcce3f487d37a98c0237992b9f281db25dd
David Riccitelli <david@wordlift.io>
2025-12-07 19:13:37 +0100

    revert to npm

--- .bin/rebuild-api.sh
+++ .bin/rebuild-api.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
-pnpm run docusaurus clean-api-docs agent && pnpm run docusaurus gen-api-docs agent
-pnpm run docusaurus clean-api-docs analysis && pnpm run docusaurus gen-api-docs analysis
-pnpm run docusaurus clean-api-docs audit && pnpm run docusaurus gen-api-docs audit
-pnpm run docusaurus clean-api-docs classification && pnpm run docusaurus gen-api-docs classification
-pnpm run docusaurus clean-api-docs content-evaluations && pnpm run docusaurus gen-api-docs content-evaluations
-pnpm run docusaurus clean-api-docs content-generation && pnpm run docusaurus gen-api-docs content-generation
-# pnpm run docusaurus clean-api-docs embeddings && pnpm run docusaurus gen-api-docs embeddings
-pnpm run docusaurus clean-api-docs events && pnpm run docusaurus gen-api-docs events
-pnpm run docusaurus clean-api-docs fact-check && pnpm run docusaurus gen-api-docs fact-check
-pnpm run docusaurus clean-api-docs graphql && pnpm run docusaurus gen-api-docs graphql
-pnpm run docusaurus clean-api-docs image-to-text && pnpm run docusaurus gen-api-docs image-to-text
-pnpm run docusaurus clean-api-docs inspector && pnpm run docusaurus gen-api-docs inspector
-pnpm run docusaurus clean-api-docs long-tail && pnpm run docusaurus gen-api-docs long-tail
-pnpm run docusaurus clean-api-docs manager && pnpm run docusaurus gen-api-docs manager
-pnpm run docusaurus clean-api-docs middleware && pnpm run docusaurus gen-api-docs middleware
-pnpm run docusaurus clean-api-docs sitemap-generator && pnpm run docusaurus gen-api-docs sitemap-generator
-pnpm run docusaurus clean-api-docs summarizer && pnpm run docusaurus gen-api-docs summarizer
+npm run docusaurus clean-api-docs agent && npm run docusaurus gen-api-docs agent
+npm run docusaurus clean-api-docs analysis && npm run docusaurus gen-api-docs analysis
+npm run docusaurus clean-api-docs audit && npm run docusaurus gen-api-docs audit
+npm run docusaurus clean-api-docs classification && npm run docusaurus gen-api-docs classification
+npm run docusaurus clean-api-docs content-evaluations && npm run docusaurus gen-api-docs content-evaluations
+npm run docusaurus clean-api-docs content-generation && npm run docusaurus gen-api-docs content-generation
+# npm run docusaurus clean-api-docs embeddings && npm run docusaurus gen-api-docs embeddings
+npm run docusaurus clean-api-docs events && npm run docusaurus gen-api-docs events
+npm run docusaurus clean-api-docs fact-check && npm run docusaurus gen-api-docs fact-check
+npm run docusaurus clean-api-docs graphql && npm run docusaurus gen-api-docs graphql
+npm run docusaurus clean-api-docs image-to-text && npm run docusaurus gen-api-docs image-to-text
+npm run docusaurus clean-api-docs inspector && npm run docusaurus gen-api-docs inspector
+npm run docusaurus clean-api-docs long-tail && npm run docusaurus gen-api-docs long-tail
+npm run docusaurus clean-api-docs manager && npm run docusaurus gen-api-docs manager
+npm run docusaurus clean-api-docs middleware && npm run docusaurus gen-api-docs middleware
+npm run docusaurus clean-api-docs sitemap-generator && npm run docusaurus gen-api-docs sitemap-generator
+npm run docusaurus clean-api-docs summarizer && npm run docusaurus gen-api-docs summarizer
